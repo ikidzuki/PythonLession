78f088a63d17d254f5cf839c0855b51e007d3401
Корепанов Альберт <59841134+ikidzuki@users.noreply.github.com>
2020-03-23 14:03:50 +0300

    deleted

--- Lession1/homework 1.2.py
+++
@@ -1,6 +0,0 @@
-time = int(input("Введите время(в секундах):"))
-
-hours = int(time / 60 // 60 % 24)
-min = time // 60 % 60
-sec = time % 60
-print(f"{hours:02}:{min:02}:{sec:02} ", sep=':')
--- Lession1/homework 1.3.py
+++
@@ -1,5 +0,0 @@
-num1 = input('Введите число:')
-num2 = num1 * 2
-num3 = num1 * 3
-sum = int(num1) + int(num2) + int(num3)
-print("Сумма чисел =", sum)
