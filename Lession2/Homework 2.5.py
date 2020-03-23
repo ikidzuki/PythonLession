903d2c79a91302ed259bed46b655d225cd00bb39
Корепанов Альберт <59841134+ikidzuki@users.noreply.github.com>
2020-03-23 13:58:55 +0300

    Добавлен 6 задание

--- Lession2/Homework 2.5.py
+++ Lession2/Homework 2.5.py
@@ -1,7 +1,10 @@
 user_num = int(input("Введите число: "))
 my_list = [3, 5, 6, 7, 7, 8]
 for i in range(len(my_list)):
-    if (user_num < my_list[i] and user_num >= my_list[i+1]) or user_num < my_list[i]:
+    if user_num < my_list[i]:
         my_list.insert(i, user_num)
+        break;
+    if my_list[-1] <= user_num:
+        my_list.insert((len(my_list)) + 1, user_num)
         break
 print(my_list)
